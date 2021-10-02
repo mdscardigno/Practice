92aa3ed7cab6356d8c74f07e01c78410dbc70f52
Milagro Scardigno <mdscardigno@gmail.com>
2021-10-02 11:17:14 -0400

    Practicing input convertion and other operations

--- Program.cs
+++ Program.cs
@@ -6,7 +6,34 @@ namespace Practice
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Welcome to C#");
+      //calculating the area of a rectangle
+      Console.WriteLine("Enter the height: ");
+      double height = Convert.ToDouble(Console.ReadLine());
+      Console.WriteLine("Enter the width: ");
+      double width = Convert.ToDouble(Console.ReadLine());
+      double area = height * width;
+      Console.WriteLine($"The rectangle area is {area}\n");
+      Console.WriteLine("Hit Enter to check the median of five numbers or the grade of a student");
+      Console.ReadKey();
+
+      //Accumulated sum
+      double accumulatedSum = 0;
+
+      //Asking user for five numbers and getting the average of the five numbers provided by the user
+      Console.WriteLine("Please enter a number: ");
+      accumulatedSum = accumulatedSum + Convert.ToDouble(Console.ReadLine());
+      Console.WriteLine("Please enter a second: ");
+      accumulatedSum = accumulatedSum + Convert.ToDouble(Console.ReadLine());
+      Console.WriteLine("Please enter a third number: ");
+      accumulatedSum = accumulatedSum + Convert.ToDouble(Console.ReadLine());
+      Console.WriteLine("Please enter a fourth number: ");
+      accumulatedSum = accumulatedSum + Convert.ToDouble(Console.ReadLine());
+      Console.WriteLine("Please enter a fifth number: ");
+      accumulatedSum = accumulatedSum + Convert.ToDouble(Console.ReadLine());
+
+      double media = accumulatedSum / 5;
+      Console.WriteLine($"The median of the five numbers provides is: {media}");
+      Console.ReadKey();
     }
   }
 }
